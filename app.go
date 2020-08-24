b61021e1e4a0683f7f629daf2069533288c31dd6
Enrique <edavalos@ucsc.edu>
2020-08-24 05:36:54 -0700

    typo

--- app.go
+++ app.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
+	"log"
 )
 
 type App struct {
@@ -11,6 +12,16 @@ type App struct {
 	DB     *sql.DB
 }
 
-func (a *App) Initialize(user, password, dbname string) { }
+func (a *App) Initialize(user, password, server, dbname string) {
+	connectionString := "postgres://" + user + ":" + password + "@" + server + "/" + dbname + "?sslmode=disable"
 
-func (a *App) Run(addr string) { }
+	var err error
+	a.DB, err = sql.Open("postgres", connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	a.Router = mux.NewRouter()
+}
+
+func (a *App) Run(addr string) {}
